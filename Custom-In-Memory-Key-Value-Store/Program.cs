997fb258c05095a361ac4a345e795f5246b30816
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-15 10:01:16 +0600

    Implement REPL loop for Mini Redis Server to handle user commands

--- Custom-In-Memory-Key-Value-Store/Program.cs
+++ Custom-In-Memory-Key-Value-Store/Program.cs
@@ -11,7 +11,13 @@ namespace Custom_In_Memory_Key_Value_Store
         {
             Console.WriteLine("==== Mini Redis Server Started ====");
             Console.WriteLine("Commands: SET [key] [value] | GET [key] | DEL [key] | EXIT");
-            
+
+            // REPL
+            while (true)
+            {
+                Console.Write("127.0.0.1:6379> ");
+                string input = Console.ReadLine();
+            }
         }
     }
 }
