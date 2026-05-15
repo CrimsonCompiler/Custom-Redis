370cd3e14bb395d5b02e1cb971d5d6d3a7913394
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-15 08:46:59 +0600

    Add project files.

---
+++ Custom-In-Memory-Key-Value-Store/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Custom_In_Memory_Key_Value_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
