6e2ede124b0fc2a1ece39d265d9d397d598106a5
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-15 09:54:42 +0600

    Initialize in-memory key-value store with a static dictionary

--- Custom-In-Memory-Key-Value-Store/Program.cs
+++ Custom-In-Memory-Key-Value-Store/Program.cs
@@ -5,9 +5,11 @@ namespace Custom_In_Memory_Key_Value_Store
 {
     internal class Program
     {
+        static Dictionary<string, string> store = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
-            
+                
         }
     }
 }
